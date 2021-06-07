05520bdef9e850f38efc6bcc7d2196eb60d03c24
Mp6 <matteo.pulcini19@gmail.com>
2021-06-07 16:57:27 -0400

    First Commit

---
+++ web_blocker.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+from datetime import datetime as dt;
+from tkinter import *
+import tkinter
+
+hosts_temp = "hosts"
+hosts_path = r"/private/etc/hosts"
+redirect = "127.0.0.1"
+website_list=[]
+
+master= tk.Tk()
+master.title("Website Blocker")
+
+#Create List
+listBox = tkinter.Listbox(master)
+listBox.pack()
+
+#Create textbox
+entry = tk.Entry (master) 
+entry.pack()
+
+def list_add():
+    website_list.append(entry.get())
+    print(website_list)
+    listBox.insert(END, entry.get())
+    entry.delete(0,END)
+
+def list_remove():
+    listBox.delete(listBox.curselection())
+
+def block():
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("working hours")
+        with open(hosts_path, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        print("non working hours")
+        with open(hosts_path, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+    
+    master.after(90000, block)
+
+button_add = tk.Button(master, text ="Add Website", command=list_add) 
+button_add.pack()
+
+button_remove = tk.Button(master, text ="Remove Website", command=list_remove)
+button_remove.pack()
+
+button_block = tk.Button(master, text ="Block", command=block) 
+button_block.pack()
+
+master.mainloop()
